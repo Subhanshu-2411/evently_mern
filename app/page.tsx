7885959969d73bd3a2ca45f23a783aa6ef74d1ad
Subhanshu Bansal <subhanshu20135@iiitd.ac.in>
2023-12-20 23:04:46 +0530

    Basic Setup Done

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,8 @@ import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main>
-      <Button
-        variant="destructive"
-        className="px-10"
-      >
-        Hello
-      </Button>
-    </main>
-  )
+    return (
+        <main>
+        </main>
+    )
 }
